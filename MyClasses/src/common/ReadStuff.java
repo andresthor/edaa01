821b25cd4e88c1be074011a0a86fcbd8df039845
andresthor <andresthor@gmail.com>
2014-02-05 23:50:44 +0100

    Added option rnd to getIntString

--- MyClasses/src/common/ReadStuff.java
+++ MyClasses/src/common/ReadStuff.java
@@ -6,8 +6,9 @@ public class ReadStuff {
 	
 	/**
 	 * Asks the user to input integers, followed by ENTER. Will also
-	 * accept # as a valid input.
-	 * @return string that contains an integer or #
+	 * accept # and rnd as valid inputs. rnd will generate a random
+	 * integer between 1 and 1000
+	 * @return string that contains an integer (input or random), or #
 	 */
 	public static String getIntString() {
 		Scanner scanner = new Scanner(System.in);
@@ -19,6 +20,10 @@ public class ReadStuff {
 				break;
 			} catch (Exception e) {
 				if (input.length() == 1 && input.charAt(0) == '#') {
+					break;
+				} else if (input.equals("rnd")) {
+					input = "" + ((int) (Math.random() * 1000));
+					System.out.println("You chose a random int and got: " + input);
 					break;
 				} else {
 					System.out.println("That was not a valid integer: ");
